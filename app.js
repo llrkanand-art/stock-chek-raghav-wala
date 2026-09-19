eef9666ff17c1016f4bfa82567376d1658ce4acf
llrkanand-art <llrkanand@gmail.com>
2026-09-19 10:53:36 -0500

    Align app with gv-chek-paid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (() => {
   const MAX_PRODUCTS = 50;
-  const PARALLEL = 25;
+  const PARALLEL = 50;
   const BATCH_TIMEOUT_MS = 8000;
   const DEVICE_KEY = 'croma_stock_signal_device_id_v1';
   const DEVICE_COOKIE = 'croma_stock_signal_device_id_v1';
